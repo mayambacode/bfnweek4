e9c9f70a46758f539a305799f22869bea675aa77
mayambacode <bokambma@sheridancollege.ca>
2024-08-02 23:49:34 -0400

    chore: Add Next.js configuration files and dependencies

---
+++ src/app/pages/login.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'json-web-token';
+
+const SECRET_KEY = process.env.SECRET_KEY || 'secret';
+
+export default  async function login(req: NextApiRequest, res: NextApiResponse){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
