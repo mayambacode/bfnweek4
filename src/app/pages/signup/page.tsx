5e4a853ab9d51c723e2a9f6eaa4f98c55049169f
mayambacode <bokambma@sheridancollege.ca>
2024-08-06 14:01:59 -0400

    refactor: Add sign up page with form and input fields

---
+++ src/app/pages/signup/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function SignUpPage() {
+    return (
+        <div>
+            <h1>Sign Up Page</h1>
+
+            <section>
+                <form>
+                    <input type="text" name="email" id="" placeholder="Email"/>
+                    <input type="password" name="password" id="" placeholder="Password" />
+                    <button type="submit"></button>
+                </form>
+            </section>
+        </div>
+    );
+}
